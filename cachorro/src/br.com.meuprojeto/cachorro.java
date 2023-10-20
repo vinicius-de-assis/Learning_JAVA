34ac0101557a472de5ee93fe6a1636743bc46aad
Vinícius de Assis Madruga Prestes <104784274+vinicius-de-assis@users.noreply.github.com>
2023-10-20 00:10:08 -0300

    ok

---
+++ cachorro/src/br.com.meuprojeto/cachorro.java
@@ -0,0 +1,10 @@
+package br.com.meuprojeto;
+
+public class cachorro {
+	int tamanho;
+	String raça;
+
+	void latir() {
+		System.out.println("Au, Au, Au, Au!!!!!");
+	}
+}
---
+++ cachorro/src/br.com.meuprojeto/cachorroteste.java
@@ -0,0 +1,12 @@
+package br.com.meuprojeto;
+
+public class cachorroteste {
+
+	public static void main(String[] args) {
+		
+		cachorro beagle = new cachorro();
+		beagle.raça = "Beagle";
+		beagle.tamanho = 40;
+		beagle.latir();		
+	}
+}
